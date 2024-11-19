7bc8393376942cdf9c986a5ba9bd5ee3d5a56980
jschl34 <121570119+jschl34@users.noreply.github.com>
2024-11-18 19:56:48 -0800

    update to cleaning and test files

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,7 +33,8 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'])
     df = df[idx].copy()
     # TODO: add code to fix the issue happened when testing the model
-    
+
+
 
     # Save the cleaned data
     logger.info('Saving and exporting cleaned data.')
@@ -54,43 +55,43 @@ if __name__ == "__main__":
   
     parser.add_argument(
         "--input_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = str,
+        help = "Input file to be cleaned",
         required = True
     )
 
     parser.add_argument(
         "--output_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = str,
+        help = "Name of clean file",
         required = True
     )
 
     parser.add_argument(
         "--output_type", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = str,
+        help = "The type for the output artifact",
         required = True
     )
 
     parser.add_argument(
         "--output_description", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = str,
+        help = "Description of the output artifact",
         required = True
     )
 
     parser.add_argument(
         "--min_price", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = float,
+        help = "The minimum price in artifact",
         required = True
     )
 
     parser.add_argument(
         "--max_price",
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = float,
+        help = "The maximum price in the artifact",
         required = True
     )
 
