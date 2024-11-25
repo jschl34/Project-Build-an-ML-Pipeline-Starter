39bbc519398d650e33e0827159bebcbd39713bdf
jschl34 <121570119+jschl34@users.noreply.github.com>
2024-11-25 08:44:19 -0800

    updating conda file

--- main.py
+++ main.py
@@ -71,9 +71,9 @@ def go(config: DictConfig):
                 parameters={
                     "csv": "clean_sample.csv:latest",
                     "ref": "clean_sample.csv:reference",
-                    "kl_threshold": config["data_check"]["kl_threshold"],
-                    "min_price": config["data_check"]["min_price"],
-                    "max_price": config["data_check"]["max_price"]
+                    "kl_threshold": config['data_check']['kl_threshold'],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
                 },
             )
 
