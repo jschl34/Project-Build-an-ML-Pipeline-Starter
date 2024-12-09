c2eb2e440bc0c8ab796814634f7a6d8950479601
jschl34 <121570119+jschl34@users.noreply.github.com>
2024-12-08 19:29:16 -0800

    whitespace change

--- main.py
+++ main.py
@@ -114,11 +114,6 @@ def go(config: DictConfig):
             )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/test_regression_model",
                 'main',
